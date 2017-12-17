ab2773a7af6f89ca929bb59e725418752f1849fc
Jeff Martin <jeffmartin@gmail.com>
2017-12-16 21:25:19 -0500

    add another template

---
+++ src/timer_templates/workout4.js
@@ -0,0 +1,71 @@
+export default {
+  name: 'pushups and crab walks',
+  phases: [
+    {
+      label: 'Warm up',
+      steps: [
+        { color: '#f4a641', label: 'arm circle steps', time: 30 },
+        { color: '#f4a641', label: 'arm cross steps', time: 30 },
+        { color: '#f4a641', label: 'high knee march', time: 30 },
+        { color: '#f4a641', label: 'Jumping Jacks', time: 30 },
+        { color: '#f4a641', label: 'up and outs', time: 30 },
+        { color: '#f4a641', label: 'Boxer shuffle', time: 30 },
+      ],
+    },
+
+    {
+      label: 'get ready',
+      steps: [{ color: '#f4a641', label: 'get ready', time: 30 }],
+    },
+
+    {
+      label: 'workout',
+      repeats: 3,
+      steps: [
+        { color: '#6ef442', label: 'pushup', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'monkey walks', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'crab walks', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'rotating planks', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'reverse bridge', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 30 },
+      ],
+    },
+
+    {
+      label: 'Water Break',
+      steps: [{ color: '#f4a641', label: 'Rest', time: 60 }],
+    },
+
+    {
+      label: 'phase 3',
+      repeats: 3,
+      steps: [
+        { color: '#6ef442', label: 'Plank', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'Side Plank', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'Side Plank Alt', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 10 },
+        { color: '#6ef442', label: 'Back Plank', time: 30 },
+        { color: '#f44741', label: 'Rest', time: 30 },
+      ],
+    },
+
+    {
+      label: 'cool down',
+      steps: [
+        { color: '#f4a641', label: 'inner thigh reachovers', time: 30 },
+        { color: '#f4a641', label: 'inner thigh reachovers alt', time: 30 },
+        { color: '#f4a641', label: 'quad stretch', time: 30 },
+        { color: '#f4a641', label: 'quad stretch alt', time: 30 },
+        { color: '#f4a641', label: 'hamstring stretch', time: 30 },
+        { color: '#f4a641', label: 'calf stretch', time: 30 },
+        { color: '#f4a641', label: 'calf stretch alt', time: 30 },
+      ],
+    },
+  ],
+}
